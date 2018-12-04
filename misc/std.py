55feb5e3c5c431007a953c478c02eb400b3dbfac
Kevin Kaland <admin@wizonesolutions.com>
2018-12-04 17:40:58 +0100

    Switch phrase back to say.

--- misc/std.py
+++ misc/std.py
@@ -87,7 +87,7 @@ ctx = Context("input")
 
 ctx.keymap(
     {
-        "phrase <dgndictation> [over]": text,
+        "say <dgndictation> [over]": text,
         "sentence <dgndictation> [over]": sentence_text,
         "comma <dgndictation> [over]": [", ", text],
         "period <dgndictation> [over]": [". ", sentence_text],
